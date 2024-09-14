e095dd8a76398bfcba93e723c5a359bede5009f7
Savita Rao <savitarao702@gmail.com>
2024-09-14 10:23:30 +0530

    image html

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let heading = document.querySelector("h1")
 let backbutton = document.querySelector(".back-button")
 let nextbutton = document.querySelector(".next-button")
-let array = ["images/butterfly1.jpeg", "images/butterfly2.jpeg", "images/buttefly3.jpeg", "images/butterfly4.jpg", "images/butterfly5.jpg", "images/buttefly6.jpeg"]
+let array = ["images/nature2.jpeg","images/nature3.jpeg","images/nature4.jpeg", "images/nature5.jpeg", "images/nature6.jpeg", "images/nature7.jpeg", "images/nature9.jpeg", "images/nature10.jpeg", "images/nature11.jpeg", "images/nature12.jpeg", "images/nature13.jpeg", "images/nature14.jpeg", "images/nautre15.jpeg"]
 let image = document.querySelector(".img")
 let i = 0;
 let text = ["1", "2", "3", "4", "5"]
 nextbutton.addEventListener("click", () => {
-    // console.log("Hello")
+    console.log("Hello")
     i++;
     if (i >= array.length) i = 0;
     image.setAttribute("src", array[i]);
