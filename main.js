31ed879f7762ca00fa7981cec39700eb9b35023b
Savita Rao <savitarao702@gmail.com>
2024-09-15 17:40:32 +0530

    some changes html css and js

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 let heading = document.querySelector("h1")
 let backbutton = document.querySelector(".back-button")
 let nextbutton = document.querySelector(".next-button")
-let array = ["images/nature2.jpeg","images/nature3.jpeg","images/nature4.jpeg", "images/nature5.jpeg", "images/nature7.jpeg", "images/nature9.jpeg", "images/nature10.jpeg","images/nature12.jpeg", "images/nature13.jpeg", "images/nature14.jpeg", "images/nautre15.jpeg", "images/nature16.jpeg"]
+let array = ["images/nature2.jpeg", "images/nature3.jpeg", "images/nature4.jpeg", "images/nature5.jpeg", "images/nature7.jpeg", "images/nature9.jpeg", "images/nature10.jpeg", "images/nature12.jpeg", "images/nature13.jpeg", "images/nature14.jpeg", "images/nautre15.jpeg", "images/nature16.jpeg"]
 let image = document.querySelector(".img")
 let i = 0;
 let text = ["1", "2", "3", "4", "5"]
+
+
+function randomColor() {
+    let colors = ["yellow", "pink", "wheat", "aqua", "orange"]
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+
 nextbutton.addEventListener("click", () => {
     console.log("Hello")
     i++;
@@ -12,15 +19,7 @@ nextbutton.addEventListener("click", () => {
     image.setAttribute("src", array[i]);
     image.setAttribute("alt", text[i]);
     console.log(image)
-
-    function randomColor (){
-        let colors = ["yellow", "pink", "wheat","aqua", "orange"]
-        return colors[Math.floor(Math.random()*colors.length)]
-    }
-    
-    nextbutton.addEventListener("click",()=>{
-        document.body.style.backgroundColor = randomColor();
-    })
+    document.body.style.backgroundColor = randomColor();
 })
 backbutton.addEventListener("click", () => {
     // console.log("Hello")
@@ -29,24 +28,7 @@ backbutton.addEventListener("click", () => {
     image.setAttribute("src", array[i]);
     image.setAttribute("alt", text[i]);
     console.log(image)
-
-    function randomColor (){
-        let colors = ["yellow", "pink", "wheat","aqua", "orange"]
-        return colors[Math.floor(Math.random()*colors.length)]
-    }
-    
-    backbutton.addEventListener("click",()=>{
-        document.body.style.backgroundColor = randomColor();
-    })
+    document.body.style.backgroundColor = randomColor();
 })
-// function randomColor (){
-//     let colors = ["yellow", "pink", "wheat","aqua", "orange"]
-//     return colors[Math.floor(Math.random()*colors.length)]
-// }
-
-// document.addEventListener("click",()=>{
-//     document.body.style.backgroundColor = randomColor();
-// })
-
 
 
